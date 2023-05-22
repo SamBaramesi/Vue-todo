79856b99d7cf949a95928a46375dcdcc2508fd1c
SamBaramesi <200750@student.glu.nl>
2023-05-22 10:17:46 +0200

    mini update

--- src/components/GreetingSection.vue
+++ src/components/GreetingSection.vue
@@ -1,17 +1,29 @@
 <template>
     <section class="greeting">
-        <h2 class="title">What's up, <input type="text" id="name" placeholder="Name here" v-model="name"></h2>
+      <h2 class="title">
+        What's up,
+        <input type="text" id="name" placeholder="Name here" v-model="name" @input="saveName">
+      </h2>
     </section>
-</template>
-
-<script>
-
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: "GreetingSection",
     data() {
-        return {
-            name: ""
-        }
+      return {
+        name: ""
+      }
     },
-}
-</script>
+    mounted() {
+      // Retrieve the name from localStorage
+      this.name = localStorage.getItem("name") || "";
+    },
+    methods: {
+      saveName() {
+        // Save the name to localStorage
+        localStorage.setItem("name", this.name);
+      }
+    }
+  }
+  </script>
